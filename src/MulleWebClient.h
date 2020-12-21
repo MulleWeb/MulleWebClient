bf08db5404085f55658920c09c60b4312082b2da
Nat! <nat@codeon.de>
2020-12-21 14:25:27 +0100

    version and RELEASENOTES

--- src/MulleWebClient.h
+++ src/MulleWebClient.h
@@ -7,7 +7,7 @@
  *
  *  version:  major, minor, patch
  */
-#define MULLE_WEB_CLIENT_VERSION  ((0 << 20) | (0 << 8) | 3)
+#define MULLE_WEB_CLIENT_VERSION  ((0 << 20) | (0 << 8) | 4)
 
 
 static inline unsigned int   MulleWebClient_get_version_major( void)
