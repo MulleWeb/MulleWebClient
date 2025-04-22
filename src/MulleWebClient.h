565a0dd6b8c5a5d6f5e7a163239c4cf706f4db08
Nat! <nat@mulle-kybernetik.com>
2025-04-22 21:18:47 +0200

    up da version and minor edits

--- src/MulleWebClient.h
+++ src/MulleWebClient.h
@@ -7,7 +7,7 @@
  *
  *  version:  major, minor, patch
  */
-#define MULLE_WEB_CLIENT_VERSION  ((0UL << 20) | (0 << 8) | 11)
+#define MULLE_WEB_CLIENT_VERSION  ((0UL << 20) | (0 << 8) | 12)
 
 
 static inline unsigned int   MulleWebClient_get_version_major( void)
